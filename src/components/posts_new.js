3c6a23bda01f42f65edb624460a998a69e0ef550
karimsamir <karim5977@gmail.com>
2017-10-15 20:21:46 +0200

    140 conditonal styling

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -5,9 +5,9 @@ import { Field, reduxForm } from 'redux-form';
 class PostsNew extends Component {
 
   renderField(field){
-
-  // const {meta : {touched, error }} = field;
-  const className = `form-group ${field.meta.touched && field.meta.error ? 'has-danger': ''}`
+// do short cut for field.meta.error and field.meta.touched
+  const {meta : {touched, error }} = field;
+  const className = `form-group ${touched && error ? 'has-danger': ''}`
     return(
       <div className={className}>
       <label>{field.label}</label>
@@ -16,7 +16,7 @@ class PostsNew extends Component {
           {...field.input}
         />
         <div className="text-help">
-          {field.meta.touched ? field.meta.error: ''}
+          {touched ? error: ''}
         </div>
       </div>
     );
