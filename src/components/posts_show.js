3b3dc1da62586bc683e798a61cbbb7574c5ae69e
karimsamir <karim5977@gmail.com>
2017-10-18 02:20:40 +0200

    Show Posts

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import fetchPost from '../actions';
+import { Link } from 'react-router-dom';
+import {fetchPost} from '../actions';
 
 class PostsShow extends Component {
 
   componentDidMount(){
-    const { id } = this.props.match.params.id;
+
+    const {id} = this.props.match.params;
     this.props.fetchPost(id);
   }
 
   render() {
+    const { post } = this.props;
 
+    if(!post){
+      return <div>Loading.....</div>;
+    }
     return (
       <div>
-        Posts Show!
+        <Link to="/">Back to index</Link>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
 }
 
 function mapStateToProps( { posts }, ownProps ){
-  return {post: posts[ownProps.match.params.id] }
+  return { post: posts[ownProps.match.params.id] }
 }
 
 export default connect(mapStateToProps, { fetchPost } )(PostsShow);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,4 +1,4 @@
-import {FETCH_POSTS} from '../actions';
+import {FETCH_POSTS, FETCH_POST} from '../actions';
 import _ from 'lodash';
 
 export default function(state = {}, action){
@@ -6,7 +6,13 @@ export default function(state = {}, action){
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
       break;
-
+    case FETCH_POST:
+      // const post = action.payload.data;
+      // const newState =  { ...state, };
+      // newState[post.id] = post;
+      // return newState;
+      return { ...state, [action.payload.data.id]: action.payload.data};
+      break;
       // case CREATE_POST:
       //   return _.mapKeys(action.payload.data, 'id');
       //   break;
